d62f63305aae691d4bf6fd82e10211b9040b0ee7
afsanaparveen <106251967+afsanaparveen@users.noreply.github.com>
2022-06-28 18:40:04 +0530

    ts2

---
+++ union.ts
@@ -0,0 +1,10 @@
+//declaring more than one datatype to a var is called union types
+let data:number=100
+console.log(data);
+//data="anil";shows error 
+let type:number|string|boolean=100
+type="anil";
+console.log(type);
+//interface is a templet
+let value={name:"bruce",age:30};
+console.log(value);
