173460fb69a871778c47aac5b5e228b69af5df0b
afsanaparveen <106251967+afsanaparveen@users.noreply.github.com>
2022-07-19 16:12:07 +0530

    form in react

---
+++ app4.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+const App = () => {
+
+    const [name, setName] = useState("");
+    const [fullName,setFullName] =useState("");
+
+
+    const inputEvent = (event) => {
+        console.log(event.target.value);
+        setName(event.target.value);
+    };
+    const onSubmit = () => {
+        setFullName(name);
+    };
+   return(
+    <>
+    <div>
+        <h1>
+            HELLO {fullName}
+
+        </h1>
+        <input type='text' placeholder="Enter your Name" onChange={inputEvent} value={name}/>
+     <button onClick={onSubmit}> clickme </button>
+    </div>
+    </>);
+}
+export default App;
